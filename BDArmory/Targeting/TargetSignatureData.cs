60a7415420227cb77f60c4accf48bb0a78d443c3
mm <megaferrari1@gmail.com>
2023-06-05 00:13:14 -0300

    Fix

--- BDArmory/Targeting/TargetSignatureData.cs
+++ BDArmory/Targeting/TargetSignatureData.cs
@@ -197,8 +197,8 @@ namespace BDArmory.Targeting
 
         public static TargetSignatureData dataLinkNoTarget(Vector3 mlPos, float maxRange, float activeRadarRange)
         {
-            Vector3 targetPos = Vector3.zero;
-            if (maxRange < (activeRadarRange * 2)) targetPos = mlPos + (maxRange * mlPos.normalized);
+            Vector3 targetPos;
+            if (maxRange > (activeRadarRange * 2)) targetPos = mlPos + (maxRange * mlPos.normalized);
             else targetPos = mlPos + ((maxRange + activeRadarRange * 2) * mlPos.normalized);
 
             return new TargetSignatureData(Vector3.zero, targetPos, Vector3.zero, true, (float)RadarWarningReceiver.RWRThreatTypes.None);
